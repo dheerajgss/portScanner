3dc9758dd3e49e6146becf8ec37c335e22b01a45
Dheeraj GSS <44668125+dheerajgss@users.noreply.github.com>
2019-09-12 22:48:30 +0530

    scanning first 1000 ports

--- portScan1.py
+++ portScan1.py
@@ -1,13 +1,16 @@
 import socket
+from termcolor import colored
 
 hold = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket.setdefaulttimeout(1)
 
 host = input("Enter host IP address to scan: ")
-port = int(input("Enter port number: "))
 
 def portscanner(port):
     if(hold.connect_ex((host,port))):
-        print("Port ",port," is closed!")
+        print(colored("Port %d is closed!" % (port),'red'))
     else:
-        print("Port ",port, " is open!")
-portscanner(port)
+        print(colored("Port %d is open!" % (port),'green'))
+
+for port in range(1,1001):
+    portscanner(port)
