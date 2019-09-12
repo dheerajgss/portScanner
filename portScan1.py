e66ff0cb8da7f0f4d28e2a78ff2056189e7457ed
Dheeraj GSS <44668125+dheerajgss@users.noreply.github.com>
2019-09-12 19:57:39 +0530

    added dynamic input for host and port

--- portScan1.py
+++ portScan1.py
@@ -2,8 +2,8 @@ import socket
 
 hold = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-host = "192.168.29.238"
-port = 21
+host = input("Enter host IP address to scan: ")
+port = int(input("Enter port number: "))
 
 def portscanner(port):
     if(hold.connect_ex((host,port))):
